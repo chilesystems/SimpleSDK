1cfe7519f92d29807a5e3b2d3a15e2672fe032e1
Gonzalo Bustamante B <mercocha13@hotmail.com>
2021-11-25 13:23:02 -0300

    Se ajusta URL de api rest

--- Services/ApiBase.cs
+++ Services/ApiBase.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SimpleSDK.Services
+﻿namespace SimpleSDK.Services
 {
     public static class ApiBase
     {
-        public const string Url = "https://api.chilesystems.com/api";
+        public const string Url = "https://simpleapi.chilesystems.com/api/v1/";
     }
 }
