862a6a20be3c2fdb53e7b0e50f3092cd24fed68f
carlosperea14 <carlos.perea@inacapmail.cl>
2024-12-16 11:32:59 -0300

    se incorporo el enpoint de listar comunas

---
+++ Models/BHE/RegionComuna.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BHE.Models
+{
+    public class RegionesData
+    {
+        [JsonProperty("Regiones")]
+        public List<Region> Regiones { get; set; }
+    }
+
+    public class Region
+    {
+        [JsonProperty("Nombre")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("Comunas")]
+        public List<string> Comunas { get; set; }
+    }
+}
